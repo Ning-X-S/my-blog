ffea1cc8b57825983dcb6b6e6fc7047e9cb91e5a
higo <higo@higodeMacBook-Pro.local>
2020-04-09 17:01:42 +0800

    ADD: 记一点

--- utils/js/utils.js
+++ utils/js/utils.js
@@ -89,6 +89,23 @@ var eventUtil = {
                 func();
             }
         }
+    },
+
+    // 移动端回到顶部
+    goTop: function() {
+      let timer = null
+      cancelAnimationFrame(timer)
+      timer = requestAnimationFrame(function fn () {
+        var oTop =
+          document.body.scrollTop || document.documentElement.scrollTop
+        if (oTop > 0) {
+          document.body.scrollTop = document.documentElement.scrollTop =
+            oTop - 50
+          timer = requestAnimationFrame(fn)
+        } else {
+          cancelAnimationFrame(timer)
+        }
+      })
     }
 }
 
